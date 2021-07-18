cf6e52947d7151aa1ae5167cd39f3a19fb495fdd
cjy324 <juy32400@gmail.com>
2021-07-18 19:28:28 +0900

    21.07.18 분할 파일 임시 파일에 쓰기 완료, 뒷처리 구현 필요

--- src/main/java/controller/UploadController.java
+++ src/main/java/controller/UploadController.java
@@ -48,30 +48,33 @@ public class UploadController {
 			// 분할 파일인 경우 
 			if(isSliced != null && isSliced.equals("true")) {
 				
+				// 파일에 대한 정보를 parameter로 받기
 				String guid = request.getParameter("guid");
 				int originSize = Integer.parseInt(request.getParameter("size"));
 				String originType = request.getParameter("type");
 				int index = Integer.parseInt(request.getParameter("index"));
 				String length = request.getParameter("length");
 				
+				// (테스트용)
 				System.out.println("guid : " + guid);
 				System.out.println("originSize : " + originSize);
 				System.out.println("originType" + originType);
 				System.out.println("index : " + index);
 				System.out.println("arrayLength : " + length);
 				
-				
+				// 분할 파일을 담을 임시 파일 생성
 				String tempPath = realPath + "\\" + guid;
 				File temp = new File(tempPath);
+				// seekpoint정보를 임시 저장해 놓을 txt파일 생성
 				String tempPathPath = realPath + "\\" + guid + ".txt";
 				File tempPathTxt = new File(tempPathPath);
 								
-				if(index == 0) {
-					temp.createNewFile(); // 1. 실제 분할 파일들을 담을 임시 파일 생성(최초 1회)
+				if(index == 0) { // 만약, 들어온 분할 파일이 첫번째 분할 파일일 경우
+					temp.createNewFile(); // 실제 분할 파일들을 담을 임시 파일 생성(최초 1회)
 					tempPathTxt.createNewFile(); // seekpoint정보를 임시 저장해 놓을 txt 파일
 				}
 				
-				System.out.println("tempPath : " + temp.getPath());
+				System.out.println("tempPath : " + temp.getPath()); //(테스트용)
 				
 				// Multipart로 요청 받기 위한 객체 생성
 				// temp파일 개념으로 분할 파일 생성
@@ -82,61 +85,63 @@ public class UploadController {
 								encType, // 인코딩 방식 지정
 								new DefaultFileRenamePolicy() // 중복 파일 처리(동일한 파일명이 업로드되면 뒤에 숫자 등을 붙여 중복 회피)
 							);
-				// 각 파일별 이름 받아오기
+				// 각 파일별 이름 받아오기(테스트용)
 				String fileName = multiReq.getFilesystemName("slicedFiles");				 
 				System.out.println(fileName);
 				
-				// 새로 들어온 분할 파일 정보 읽기
+				// 새로 들어온 분할 파일 객체 생성
 				File newFile = multiReq.getFile("slicedFiles");
 			
-				System.out.println("newFile : " + newFile.getName());
+				System.out.println("newFile : " + newFile.getName()); //(테스트용)
 			
+				// 새로 들어온 분할 파일 읽기
 				FileInputStream fr = new FileInputStream(newFile);
-				int read;
-				byte[] buffer = new byte[1024]; // 파일 내용을 담을 버퍼(?) 선언
-				String stringData = null;
-				StringBuilder data = new StringBuilder();
-				while((read = fr.read(buffer)) != -1) {
-					stringData = new String(buffer, 0, read);
-					//System.out.println(stringData);
-					data.append(stringData);
-				}
-				System.out.println("data.length() : " + data.length());
-				
-				// read/write 랜덤 액세스 파일 열기
+				int fileByte;
+				// RandomAccessFile로 분할 파일들을 담을 임시파일 객체 생성(read/write)
 				RandomAccessFile raf = new RandomAccessFile(temp, "rw"); 
 				
-				long seekPoint = 0;
+				long seekPoint = 0; // seekPoint 설정
 				
+				// 만약, 기존에 seekpoint정보를 임시 저장해 놓은 txt파일이 존재할 경우..
 				// 임시 txt파일에 저장해둔 seekPoint 값 가져오기
 				if(tempPathTxt.exists()) {
-//					String test = Files.readString(tempPathTxt.toPath());
 					String getSeekPoint = "";
 					BufferedReader br = new BufferedReader(new FileReader(tempPathTxt));
 					
 					while((getSeekPoint = br.readLine()) != null) {
 						seekPoint = Long.parseLong(getSeekPoint);
 					}
-					br.close();
+					br.close(); // 자원 사용 종료
 				}
 				
-				raf.seek(seekPoint); // seekPoint 설정
-				raf.writeBytes(data.toString()); // 파일쓰기
+				// seekPoint 설정
+				raf.seek(seekPoint); 
+
+				// 1바이트씩 읽으면서 파일쓰기
+				while((fileByte = fr.read()) != -1) {
+					raf.write(fileByte); // 파일쓰기
+				}
+//				System.out.println("data.length() : " + data.length());
+				System.out.println("fileByte : " + fileByte); //(테스트용)
 				
-				System.out.println("getFilePointer : " + raf.getFilePointer());
-				System.out.println("seekPoint : " + seekPoint);
-				System.out.println("-------------------------------------------");
 				
+				// 현재 파일을 쓴 바이트 크기만큼 seekPoint 값 증가
+				long addedSeekPoint = seekPoint + newFile.length();
 				// 현재 분할 파일의 데이터 크기만큼 seekPoint를 증가시키고 
-				// 이 정보를 임시 .txt파일에 기록
+				// 이 정보를 임시 txt파일에 기록
 				if(tempPathTxt.exists()) {
 					FileOutputStream fos = new FileOutputStream(tempPathTxt);
-					fos.write(Long.toString(seekPoint + data.length()).getBytes());		
-					fos.close();
+					fos.write(Long.toString(addedSeekPoint).getBytes());
+					fos.close(); // 자원 사용 종료
 				}
 				
-				raf.close();
-				fr.close(); 
+				// (테스트용)
+				System.out.println("getFilePointer : " + raf.getFilePointer());
+				System.out.println("addedSeekPoint : " + addedSeekPoint);
+				System.out.println("-------------------------------------------");
+				
+				raf.close(); // 자원 사용 종료
+				fr.close(); // 자원 사용 종료
 
 			}else {
 				// Multipart로 요청 받기 위한 객체 생성
